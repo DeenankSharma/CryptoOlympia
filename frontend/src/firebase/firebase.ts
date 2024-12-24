12c22f854a3e75c95a44d1ded296052c39c48651
Deenank Sharma <146727112+DeenankSharma@users.noreply.github.com>
2024-12-25 05:07:23 +0530

    Updates

--- frontend/src/firebase/firebase.ts
+++ frontend/src/firebase/firebase.ts
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKqlfPOn0Fn1uUmiHch-brrKT7gFRuh4Y",
-  authDomain: "cryptoolympia.firebaseapp.com",
-  projectId: "cryptoolympia",
-  storageBucket: "cryptoolympia.firebasestorage.app",
-  messagingSenderId: "359116637321",
-  appId: "1:359116637321:web:ab11c9b2a2f351265c3258"
+  
 };
 
 export const app = initializeApp(firebaseConfig)
